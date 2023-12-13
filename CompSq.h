b2f4007e76329d0db6aeb77468dbb5830f1c0403
Kiran Kumari <86914350+KiranDLExperience@users.noreply.github.com>
2023-12-13 10:33:22 +0100

    semicolo

--- CompSq.h
+++ CompSq.h
@@ -2,7 +2,7 @@
 
 #ifndef _OTEE_H
 #define _OTEE_H
-#define MAX_N 3810777 // This defines the maximum value of n for which the code can handle the total squares value
+#define MAX_N 3810777; // This defines the maximum value of n for which the code can handle the total squares value
 #define ULLONG unsigned long long;
 
 unsigned long long CompSquares(int n); 
