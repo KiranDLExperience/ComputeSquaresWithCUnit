f8d1236a93b1a01153487d11b826da0867da75a5
Kiran Kumari <86914350+KiranDLExperience@users.noreply.github.com>
2023-12-13 19:44:47 +0100

    Unit test

--- CUnitTest.c
+++ CUnitTest.c
@@ -1,7 +1,24 @@
 #include <CUnit/Basic.h>
 #include "./header/CompSq.h"
 
-void test_function(void) {
-    ULLONG result = CompSquares(4);
-    CU_ASSERT_EQUAL(result, 30);
+void CompSquares(void) {
+  CU_ASSERT(CompSquares(1) == 1);
+  CU_ASSERT(CompSquares(4) == 30);
+  CU_ASSERT(CompSquares(0) == 0);
+  CU_ASSERT(CompSquares(3810778) == 0);
+}
+
+int main() 
+{
+  CU_pSuite pSuite = NULL;
+
+  CU_initialize_registry();
+
+  pSuite = CU_add_suite("My Suite", NULL, NULL);
+  CU_add_test(pSuite, "CompSquares", CompSquares);
+
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+
+  return CU_get_error();
 }
