60ba3adc5ed194b877d8fc0ca7030cdf7fb689dd
Kiran Kumari <86914350+KiranDLExperience@users.noreply.github.com>
2023-12-13 10:42:50 +0100

    typedef for ULONG

--- CompSq.h
+++ CompSq.h
@@ -4,7 +4,7 @@
 #define _OTEE_H
 #define MAX_N 3810777 // This defines the maximum value of n for which the code can handle the total squares value
 
-typedef (unsigned long long) ULLONG;
+typedef unsigned long long ULLONG;
 
 unsigned long long CompSquares(int n); 
 
