009d0c0503de213db9ec4c7f2948b03c5df4691a
Kiran Kumari <86914350+KiranDLExperience@users.noreply.github.com>
2023-12-13 10:38:36 +0100

    Update CompSq.h

--- CompSq.h
+++ CompSq.h
@@ -2,8 +2,9 @@
 
 #ifndef _OTEE_H
 #define _OTEE_H
-#define MAX_N 3810777; // This defines the maximum value of n for which the code can handle the total squares value
-#define ULLONG unsigned long long;
+#define MAX_N 3810777 // This defines the maximum value of n for which the code can handle the total squares value
+
+#typedef (unsigned long long) ULLONG;
 
 unsigned long long CompSquares(int n); 
 
